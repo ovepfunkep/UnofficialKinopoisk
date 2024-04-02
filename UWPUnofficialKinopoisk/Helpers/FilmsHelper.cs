1132b7529c5a1567490cb36ba9e36fbe8eb04061
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-04-02 20:59:28 +0700

    - Implemented films view

---
+++ UWPUnofficialKinopoisk/Helpers/FilmsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UWPUnofficialKinopoisk.Models;
+
+namespace UWPUnofficialKinopoisk.Helpers
+{
+    public static class FilmsHelper
+    {
+        public static string GetInfo(Film film) =>
+            $"{film?.Year}, {film?.RatingKinopoisk}, {film?.FilmLength} min.";
+    }
+}
---
+++ UWPUnofficialKinopoisk/ViewModels/FilmsCollectionViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UWPUnofficialKinopoisk.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using static UWPUnofficialKinopoisk.Services.APIService;
+using Windows.UI.Xaml.Controls;
+using UWPUnofficialKinopoisk.ControlModels;
+using static UWPUnofficialKinopoisk.Helpers.FilmsHelper;
+
+namespace UWPUnofficialKinopoisk.ViewModels
+{
+    public class FilmsCollectionViewModel : ObservableObject
+    {
+        public ObservableCollection<FilmControlModel> FilmsList { get; set; } = new ObservableCollection<FilmControlModel>();
+
+        public FilmsCollectionViewModel()
+        {
+            LoadDataAsync();
+        }
+
+        private async void LoadDataAsync(FilmCollectionsTypesEnum type = FilmCollectionsTypesEnum.TOP_POPULAR_ALL, uint page = 1)
+        {
+            try
+            {
+                var films = await GetFilmsAsync(type, page);
+
+                var filmsList = films.Select(f => new FilmControlModel(f.KinopoiskId,
+                                                                      f.PosterUrl,
+                                                                      f.NameRu,
+                                                                      GetInfo(f)));
+
+                FilmsList.Clear();
+                filmsList.ToList().ForEach(f => FilmsList.Add(f));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading films: " + ex.Message);
+            }
+        }
+    }
+}
