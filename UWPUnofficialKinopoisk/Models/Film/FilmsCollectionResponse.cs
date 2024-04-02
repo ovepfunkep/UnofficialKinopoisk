daeaf75dc89b1dcc12b5c9b963ca0a29ae5886a5
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-04-02 14:24:50 +0700

    - Implemented models

---
+++ UWPUnofficialKinopoisk/Models/Film/FilmsCollectionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPUnofficialKinopoisk.Models
+{
+    public class FilmsCollectionResponse
+    {
+        public uint Total { get; set; }
+        public uint TotalPages { get; set; }
+        public List<Film> Items { get; set; }
+    }
+}
