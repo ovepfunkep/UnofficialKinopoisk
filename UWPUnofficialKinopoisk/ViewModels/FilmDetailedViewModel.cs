0e1b9f78cb8ae4a552c203bfcec21643ad69ae50
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-04-03 20:16:35 +0700

    - Refined detailed film view page - Little fix in FilmsHelper

--- UWPUnofficialKinopoisk/ViewModels/FilmDetailedViewModel.cs
+++ UWPUnofficialKinopoisk/ViewModels/FilmDetailedViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 using UWPUnofficialKinopoisk.Models;
 using UWPUnofficialKinopoisk.Services;
+
+using Windows.UI.Xaml;
+
+using static UWPUnofficialKinopoisk.Helpers.FilmsHelper;
 
 namespace UWPUnofficialKinopoisk.ViewModels
 {
@@ -17,17 +23,37 @@ namespace UWPUnofficialKinopoisk.ViewModels
         public Film Film
         {
             get => _film;
-            set => SetProperty(ref _film, value);
+            set
+            {
+                SetProperty(ref _film, value);
+                FilmInfo = GetInfo(Film);
+            }
+        }
+        private string _filmInfo;
+        public string FilmInfo 
+        { 
+            get => _filmInfo;
+            set => SetProperty(ref _filmInfo, value);
         }
 
-        public FilmDetailedViewModel(int kinopoiskID) 
+        private readonly Type PrevPage;
+        public ICommand GoBackCommand { get; }
+
+        public FilmDetailedViewModel(int kinopoiskID, Type previousControlType = null) 
         {
             LoadFilmAsync(kinopoiskID);
+            PrevPage = previousControlType ?? typeof(FilmsCollectionPage);
+            GoBackCommand = new RelayCommand(MoveToPreviousPage);
         }
 
         private async void LoadFilmAsync(int kinopoiskID)
         {
             Film = await APIService.GetFilmAsync(kinopoiskID);
+        }
+
+        private void MoveToPreviousPage()
+        {
+            ((App)Application.Current).NavigateTo(PrevPage);
         }
     }
 }
