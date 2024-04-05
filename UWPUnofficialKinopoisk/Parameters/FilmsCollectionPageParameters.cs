03e8b6bc889dcf21e313b1c2c70a31c69a1c9685
Tyabin Ivan Alekseyevich <Ivan_Tyabin@mail.ru>
2024-04-05 12:54:25 +0700

    - Added year filtration

--- UWPUnofficialKinopoisk/Parameters/FilmsCollectionPageParameters.cs
+++ UWPUnofficialKinopoisk/Parameters/FilmsCollectionPageParameters.cs
@@ -9,10 +9,12 @@ namespace UWPUnofficialKinopoisk.Parameters
     public class FilmsCollectionPageParameters
     {
         public bool ShowOnlyFavorite { get; set; }
+        public ushort YearLowerBound { get; set; } 
 
-        public FilmsCollectionPageParameters(bool showOnlyFavorite = false)
+        public FilmsCollectionPageParameters(bool showOnlyFavorite = false, ushort yearLowerBound = 0)
         {
             ShowOnlyFavorite = showOnlyFavorite;
+            YearLowerBound = yearLowerBound;
         }
     }
 }
--- UWPUnofficialKinopoisk/ViewModels/FilmsCollectionViewModel.cs
+++ UWPUnofficialKinopoisk/ViewModels/FilmsCollectionViewModel.cs
@@ -18,22 +18,34 @@ namespace UWPUnofficialKinopoisk.ViewModels
     public class FilmsCollectionViewModel : ObservableObject
     {
         private List<FilmControlModel> FilmsList { get; set; } = new List<FilmControlModel>();
+
         private bool _showOnlyFavorites = false;
         public bool ShowOnlyFavorites
         {
             get => _showOnlyFavorites;
             set => SetProperty(ref _showOnlyFavorites, value);
         }
+        private ushort _yearLowerBound = 0;
+        public ushort YearLowerBound 
+        {
+            get => _yearLowerBound;
+            set => SetProperty(ref _yearLowerBound, value);
+        }
+
         public ObservableCollection<FilmControlModel> FilteredFilmsList { get; set; } = new ObservableCollection<FilmControlModel>();
         public ICommand ApplyFiltersCommand { get; }
 
         public void ApplyFilters()
         {
             FilteredFilmsList.Clear();
-            FilmsList.Where(f => !ShowOnlyFavorites || f.Film.IsFavorite == ShowOnlyFavorites)
+            FilmsList.Where(f => IsFilmSuits(f.Film))
                      .ToList()
                      .ForEach(f => FilteredFilmsList.Add(f));
         }
+
+        private bool IsFilmSuits(Film film) => IsFavoriteSatisfies(film.IsFavorite) && IsYearSatisfies(film.Year ?? film.StartYear);
+        private bool IsFavoriteSatisfies(bool? isFavorite) => !ShowOnlyFavorites || isFavorite == ShowOnlyFavorites;
+        private bool IsYearSatisfies(int? year) => year != null && year >= YearLowerBound;
 
         public FilmsCollectionViewModel()
         {
